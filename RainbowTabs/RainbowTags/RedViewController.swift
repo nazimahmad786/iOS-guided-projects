c45bd35a1415b708d4c90f22aeb59fcc548fa262
nazimahmad786 <102575501+nazimahmad786@users.noreply.github.com>
2024-04-08 09:07:56 +0530

    Add files via upload

---
+++ RainbowTabs/RainbowTags/RedViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RedViewController.swift
+//  RainbowTags
+//
+//  Created by Student on 04/03/24.
+//
+
+import UIKit
+
+class RedViewController: NSObject {
+
+}
---
+++ RainbowTabs/RainbowTags/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RainbowTags
+//
+//  Created by Student on 04/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tabBarItem.badgeValue = "1"
+    }
+
+//    override func viewWillDisappear(_ animated: Bool) {
+//        tabBarItem.badgeValue = nil
+//    }
+}
+
